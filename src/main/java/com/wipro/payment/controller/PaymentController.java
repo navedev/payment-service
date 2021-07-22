7cc66d972130ed995e647154678b35bb457472a6
navedev <naveen.d088@gmail.com>
2021-07-22 17:27:47 +0530

    cors

--- src/main/java/com/wipro/payment/controller/PaymentController.java
+++ src/main/java/com/wipro/payment/controller/PaymentController.java
@@ -3,6 +3,7 @@ package com.wipro.payment.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/payment")
+@CrossOrigin
 public class PaymentController {
 
 	@Autowired
